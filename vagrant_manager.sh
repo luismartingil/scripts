7d91882101569130a2c6b778f5fadeab9591d0cb
Luis Martin Gil <martingil.luis@gmail.com>
2013-06-27 23:34:09 -0400

    + Added the vagrant management script.

---
+++ vagrant_manager.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Bash script to start/stop my local vagrant boxes.
+# Author: luismartingil
+# Year: 2012
+#
+
+ROOT=/Volumes/lmartin_data/vagrant/
+ARRAY=( 
+    100ua-debian-squeeze-amd64
+    101ua-pjsua-debian-squeeze-amd64 
+    102ua-pjsua-debian-squeeze-amd64 
+    103ua-pjsua-debian-squeeze-amd64)
+    )
+
+# Let's grab the user's input param. 
+case $@ in
+    start)
+	cmd=(vagrant up)
+	;;
+    stop)
+	cmd=(vagrant halt)
+	;;
+    *)
+	echo 'Please select start/stop'
+	exit
+	;;
+esac
+
+# Now we now what to do and where.
+# Don't forget that vagrant {up, halt} MUST be sequential.
+for i in "${ARRAY[@]}"
+do
+    echo $ROOT$i
+    cd $ROOT$i
+    "${cmd[@]}"
+    echo ''
+done
