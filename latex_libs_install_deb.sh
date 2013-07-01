024d5020e10ca66a8e3402c28fe1594fe5abf912
Luis Martin Gil <martingil.luis@gmail.com>
2013-07-01 11:30:58 -0400

    + Removed another dependency.

--- latex_libs_install_deb.sh
+++ latex_libs_install_deb.sh
@@ -15,7 +15,7 @@ ARRAY=(
 # Bash functions to install dependencies.
 # Probably more deps than needed!
 install_debian_req () {
-    sudo apt-get install -y texlive-base texlive-fonts-recommended texlive-latex-base texlive-latex-recommended texify multex-bin tetex-frogg  tex-gyre  texlive-xetex texlive-pictures texlive-luatex texlive-bibtex-extra  texlive-extra-utils  texlive-font-utils texlive-fonts-extra  texlive-formats-extra texlive-generic-extra texlive-games  texlive-plain-extra texlive-latex-extra texlive-science  tshark texlive-base texlive-fonts-recommended texlive-latex-base ttf-dejavu-core ttf-gfs-* ttf-dejavu ttf-liberation ttf-bitstream-vera python-setuptools
+    sudo apt-get install -y texlive-base texlive-fonts-recommended texlive-latex-base texlive-latex-recommended texify tetex-frogg  tex-gyre  texlive-xetex texlive-pictures texlive-luatex texlive-bibtex-extra  texlive-extra-utils  texlive-font-utils texlive-fonts-extra  texlive-formats-extra texlive-generic-extra texlive-games  texlive-plain-extra texlive-latex-extra texlive-science  tshark texlive-base texlive-fonts-recommended texlive-latex-base ttf-dejavu-core ttf-gfs-* ttf-dejavu ttf-liberation ttf-bitstream-vera python-setuptools
     # Pygments is used for coloring and adding source files
     # in our LaTeX documents.
     sudo easy_install Pygments
