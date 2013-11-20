7164f790d759c44ef515b018869b4cd3a79aa912
Luis Martin Gil <martingil.luis@gmail.com>
2013-11-20 11:37:12 -0500

    + Played with dependencies

--- install_thrift.sh
+++ install_thrift.sh
@@ -13,8 +13,9 @@ WEB=http://apache.mirrorcatalogs.com/thrift/$VER_TH/$NAME.tar.gz
 # Bash functions to install dependencies.
 # Probably more deps than needed!
 install_centos_req () {
+    sudo yum install -y glib glib2 glib2-devel glibc glibc-devel glibc-common
     sudo yum groupinstall -y 'Development Tools'
-    #sudo yum install -y boost libevent libevent-devel automake.noarch libtool flex bison ustr-devel libgudev1-devel NetworkManager-glib-devel ustr-devel pm-utils-devel libgudev1-devel openssl-devel openssl-static ruby-static ruby-devel ruby-libs ruby perl-Carp-Clan rubygem-rake python-devel gstreamer-python-devel dbus-python-devel python-twisted perl-core php php-cli glib2 glib malaga automake bison bison-devel gfs2-utils doxygen glib2 automake libtool flex bison pkgconfig gcc-c++ boost-devel libevent-devel zlib-devel ruby-devel
+    #sudo yum install -y boost libevent libevent-devel automake.noarch libtool flex bison ustr-devel libgudev1-devel ustr-devel pm-utils-devel libgudev1-devel openssl-devel openssl-static ruby-static ruby-devel ruby-libs ruby perl-Carp-Clan rubygem-rake python-devel gstreamer-python-devel dbus-python-devel python-twisted perl-core php php-cli glib2 glib malaga automake bison bison-devel gfs2-utils doxygen glib2 automake libtool flex bison pkgconfig gcc-c++ boost-devel libevent-devel zlib-devel ruby-devel
 }
 install_debian_req () {
     sudo apt-get install libboost-dev libboost-test-dev libboost-program-options-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev ruby-full ruby-dev librspec-ruby rake rubygems libdaemons-ruby libgemplugin-ruby mongrel python-dev python-twisted libbit-vector-perl php5-dev php5-cli libglib2.0-dev erlang-base erlang-eunit erlang-dev mono-gmcs libmono-dev libmono-system-web2.0-cil ghc6 cabal-install libghc6-binary-dev libghc6-network-dev libghc6-http-dev automake bison global doxygen libglib2.0-0 libevent-dev automake libtool flex bison pkg-config g++ libssl-dev automake libtool flex bison pkg-config g++
@@ -76,7 +77,7 @@ cd; sudo tar -zxvf $NAME.tar.gz -C /usr/local/src
 
 # Compiling and installing the lib
 cd /usr/local/src/$NAME ; sudo chmod uog+x configure
-cd /usr/local/src/$NAME ; sudo ./configure --without-csharp --without-java --without-erlang --without-perl --without-php --without-php_extension --without-ruby --without-haskell --without-python --with-cpp --with-c_glib
+cd /usr/local/src/$NAME ; sudo ./configure --without-csharp --without-java --without-erlang --without-perl --without-php --without-php_extension --without-ruby --without-haskell --without-python --with-cpp --with-c_glib --with-c_glib=yes
 
 # Lets go!
 cd /usr/local/src/$NAME ; sudo make
