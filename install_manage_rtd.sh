03633ec695621d7f7c0be823edd73024c57f5531
Luis Martin Gil <martingil.luis@gmail.com>
2014-08-20 08:00:34 -0400

    + Fixed nginx conf

--- install_manage_rtd.sh
+++ install_manage_rtd.sh
@@ -54,6 +54,7 @@ install_configure_nginx () {
     echo 'Configuring nginx adding readthedocs server'
     sudo mkdir /etc/nginx/site-enabled
     TMP_FILE=`mktemp`
+    # Note skipping $ symbols
     cat > $TMP_FILE <<EOF
 server {
     listen 80;
@@ -63,9 +64,9 @@ server {
 
     location / {
         proxy_pass http://127.0.0.1:8000;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     }
 }
 EOF
