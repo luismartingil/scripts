3d0c1216e13e423a9fcbb0c936c589e5377fa9b7
Luis Martin Gil <martingil.luis@gmail.com>
2014-08-27 18:21:16 -0400

    + Updated comment.

--- install_manage_rtd.sh
+++ install_manage_rtd.sh
@@ -53,6 +53,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
+# You need to activate the lesssecureapps if using google's smtp
+# https://www.google.com/settings/security/lesssecureapps
 EMAIL_HOST_USER = 'TODO@gmail.com'
 EMAIL_HOST_PASSWORD = 'TODO'
 DEFAULT_FROM_EMAIL = 'admin@docs.dev.net'
