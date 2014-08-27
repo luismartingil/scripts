90c3f95a4b3b25fb09a713fce9f232b86d18522b
Luis Martin Gil <martingil.luis@gmail.com>
2014-08-27 10:42:27 -0400

    + Logging.

--- install_manage_rtd.sh
+++ install_manage_rtd.sh
@@ -354,8 +354,8 @@ case $1 in
 	qquit
 	;;
 esac
-show_logs_available
 T2=$(date +%s)
 diffsec="$(expr $T2 - $T1)"
 echo | awk -v D=$diffsec '{printf "Elapsed time: %02d:%02d:%02d\n",D/(60*60),D%(60*60)/60,D%60}'
+show_logs_available
 # =================================================
